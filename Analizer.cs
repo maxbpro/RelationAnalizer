22922dfdb722cf16169186b8c3243c64b50c01e5
maxpro <maxb_pro@mail.ru>
2012-04-03 19:52:47 +0400

    adding metod for analize 1NF

--- Analizer.cs
+++ Analizer.cs
@@ -29,6 +29,67 @@ namespace Base
             rowNum = Ar[0].Count;
             columnNum = dTable.Columns.Count;
         }
+
+        public bool IsTwoForm(List<int> keys)
+        {
+            bool flag = true;
+            List<string> keysName = new List<string>();
+            for (int j = 0; j < keys.Count; j++)
+            {
+                keysName.Add(dTable.Columns[keys[j]].ToString());
+            }
+
+            for (int i = 0; i < columnNum; i++)
+            {
+   
+                Queue<int> result = ComparePrimaryWithColumn(keys, i);
+                if (result != null)
+                {
+                    while (result.Count != 0)
+                    {
+                        int currentKey = result.Dequeue();
+                        winReplace win = new winReplace(keysName, dTable.Columns[i].ColumnName, dTable.Columns[currentKey].ColumnName);
+                        win.ShowDialog();
+                        flag = false;
+                    }
+                }
+
+
+                
+            }
+            if (flag)
+                return true;
+            else
+                return false;
+        }
+
+        // возвращает индекс ключа, от которого зависит столбец (который выносим в новое отношение с этим ключом)
+        // в случае неполной зависимости от составного ключа
+        private Queue<int> ComparePrimaryWithColumn(List<int> keys, int indexY)
+        {
+            if (keys.Contains(indexY))
+                return null;
+            List<int> list = new List<int>();
+            // Находим независимость
+            for (int i = 0; i < keys.Count; i++)
+            {
+                if (!IsFunctialDepended(keys[i], indexY))
+                    list.Add(i);
+            }
+            if (list.Count == 0)
+                return null;
+            else
+            {
+                // Находим зависимые
+                Queue<int> result = new Queue<int>();
+                for (int i = 0; i < keys.Count; i++)
+                {
+                    if (!list.Contains(keys[i]))
+                        result.Enqueue(keys[i]);
+                }
+                return result;
+            }
+        }
         
 
         private bool IsFunctialDepended(int indexX, int indexY)
