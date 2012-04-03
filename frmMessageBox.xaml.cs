a7eda662d16dfd469893146713d0d4d7fbf92848
maxpro <maxb_pro@mail.ru>
2012-04-03 18:26:27 +0400

    Classes and forms for reporting information to user

---
+++ frmMessageBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Base
+{
+    /// <summary>
+    /// Логика взаимодействия для frmMessageBox.xaml
+    /// </summary>
+    public  partial class frmMessageBox : Window
+    {
+        public  frmMessageBox(string text)
+        {
+            InitializeComponent();
+            textblock1.Text = text;
+            
+        }
+
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
